06fa9451501072a2acf4b8594fdc72919d269830
maniel <manuelpepr99@icloud.com>
2025-07-02 23:17:04 +0000

    new game utilising much on js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const boxes = document.querySelectorAll(".box")
+const boxes = document.querySelectorAll(".box");
+let gameBoard = document.getElementById('gameboard');
+let scoreBoard = document.getElementById('scoreboard');
+let reset = document.getElementById('reset');
+let stop = document.getElementById('stop');
+let currentPlayer = 'X';
+let startBtn = document.getElementById('start');
 
 boxes.forEach(box => {
-   box.addEventListener("click", ()=>{
-    if(box.textContent === ""){
-        box.textContent = "X";
-    }
-    box.classList.add("x-mark");
-   })
+    box.addEventListener('click', ()=>{
+       if(box.textContent === ""){
+        box.textContent = currentPlayer;
+        box.classList.add(currentPlayer === 'X' ? 'X-mark' : 'O-mark');
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+       
+       }
+    })
+})
+
+startBtn.addEventListener('click', ()=>{
+    gameBoard.classList.add('showGameBoard');
+    scoreBoard.style.display = "block";
+    reset.style.display = 'block';
+    stop.style.display = 'block';
 })
